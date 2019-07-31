b5339de1d81733d2debc0aa9e9d292622e0c1777
MyFutureIsAMess <1085844536@qq.com>
2019-07-31 18:26:53 +0800

    代码整合

--- src/main/java/com/zujuan/controller/protal/ShiTiController.java
+++ src/main/java/com/zujuan/controller/protal/ShiTiController.java
@@ -3,6 +3,7 @@ package com.zujuan.controller.protal;
 import com.zujuan.common.ServerResponse;
 import com.zujuan.pojo.ShiTi;
 import com.zujuan.service.IShiTiService;
+import com.zujuan.vo.ST_ZSDVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/shiti/")
 public class ShiTiController {
-    @Autowired
+    /*@Autowired
     private IShiTiService iShiTiService;
 
     @RequestMapping(value = "submit.do", method = RequestMethod.POST)
@@ -24,4 +25,13 @@ public class ShiTiController {
     public ServerResponse<ShiTi> submit(ShiTi shiTi) {
         return iShiTiService.submit(shiTi);
     }
+
+    @RequestMapping(value = "add.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<ST_ZSDVo> submit(ST_ZSDVo st_zsdVo) {
+
+        ServerResponse<ST_ZSDVo> s = iShiTiService.add(st_zsdVo);
+
+        return s;
+    }*/
 }
--- src/main/java/com/zujuan/service/impl/ShiTiServiceImpl.java
+++ src/main/java/com/zujuan/service/impl/ShiTiServiceImpl.java
@@ -3,7 +3,9 @@ package com.zujuan.service.impl;
 import com.zujuan.common.ServerResponse;
 import com.zujuan.dao.ShiTiMapper;
 import com.zujuan.pojo.ShiTi;
+import com.zujuan.pojo.ZhiShiDian;
 import com.zujuan.service.IShiTiService;
+import com.zujuan.vo.ST_ZSDVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,4 +30,22 @@ public class ShiTiServiceImpl implements IShiTiService {
         }
         return ServerResponse.createBySuccess("上传成功", shiTi);
     }
+
+    /*@Override
+    public ServerResponse<ST_ZSDVo> add(ST_ZSDVo st_zsdVo) {
+        ShiTi shiTi = new ShiTi();
+        ZhiShiDian zhiShiDian = new ZhiShiDian();
+
+        shiTi.setSource(st_zsdVo.getSource());
+        shiTi.setCity(st_zsdVo.getCity());
+        shiTi.setProblem(st_zsdVo.getProblem());
+        shiTi.setAnswer(st_zsdVo.getAnswer());
+        shiTi.setAnalysis(st_zsdVo.getAnalysis());
+        shiTi.setNandu(st_zsdVo.getNandu());
+        shiTi.setTilei(st_zsdVo.getTilei());
+        shiTi.setTixing(st_zsdVo.getTixing());
+        shiTi.setReferenceTime(0);
+        shiTi.setYears(st_zsdVo.getYears());
+        shiTiMapper.insert(shiTi);
+    }*/
 }
